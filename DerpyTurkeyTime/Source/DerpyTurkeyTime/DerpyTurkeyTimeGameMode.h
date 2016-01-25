d0fb6fd4953ccabb2eee2bac7d2075c04dd7b4a8
Amanda <asweaton@gmail.com>
2016-01-24 19:58:59 -0500

    Initial project files, main menu UI, in game UI

---
+++ DerpyTurkeyTime/Source/DerpyTurkeyTime/DerpyTurkeyTimeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DerpyTurkeyTimeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DERPYTURKEYTIME_API ADerpyTurkeyTimeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
